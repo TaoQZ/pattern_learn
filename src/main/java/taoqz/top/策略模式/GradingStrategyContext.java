0fbc9b29e23ccbf129108ad0f426d731aa5d22c6
taoqingzhou <taoqingzhou163@163.com>
2022-10-30 22:10:39 +0800

    mod 策略模式

---
+++ src/main/java/taoqz/top/策略模式/GradingStrategyContext.java
@@ -0,0 +1,21 @@
+package taoqz.top.策略模式;
+
+import java.util.List;
+
+public class GradingStrategyContext {
+
+    private GradingStrategy gradingStrategy;
+
+    public List<String> execute(User user) {
+        return gradingStrategy.getRecommendedVideos(user);
+    }
+
+
+    public GradingStrategy getGradingStrategy() {
+        return gradingStrategy;
+    }
+
+    public void setGradingStrategy(GradingStrategy gradingStrategy) {
+        this.gradingStrategy = gradingStrategy;
+    }
+}
