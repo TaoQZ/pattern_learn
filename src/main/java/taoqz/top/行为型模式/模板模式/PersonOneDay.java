ca2d14c53382f6926a3288ababf03b57a28fff61
taoqingzhou <taoqingzhou@gov110.cn>
2022-11-09 18:05:15 +0800

    add 回调

--- src/main/java/taoqz/top/行为型模式/模板模式/PersonOneDay.java
+++ src/main/java/taoqz/top/行为型模式/模板模式/PersonOneDay.java
@@ -7,17 +7,17 @@ package taoqz.top.行为型模式.模板模式;
  **/
 public class PersonOneDay extends PersonsDay{
     @Override
-    void eat() {
+    protected void eat() {
         System.out.println("我吃面食");
     }
 
     @Override
-    void sleep() {
+    protected void sleep() {
         System.out.println("我趴着睡");
     }
 
     @Override
-    void dadoudou() {
+    protected void dadoudou() {
         System.out.println("我拳打");
     }
 }
--- src/main/java/taoqz/top/行为型模式/模板模式/PersonTwoDay.java
+++ src/main/java/taoqz/top/行为型模式/模板模式/PersonTwoDay.java
@@ -7,17 +7,17 @@ package taoqz.top.行为型模式.模板模式;
  **/
 public class PersonTwoDay extends PersonsDay{
     @Override
-    void eat() {
+    protected void eat() {
         System.out.println("我吃米饭");
     }
 
     @Override
-    void sleep() {
+    protected void sleep() {
         System.out.println("我侧着睡");
     }
 
     @Override
-    void dadoudou() {
+    protected void dadoudou() {
         System.out.println("我脚踢");
     }
 }
