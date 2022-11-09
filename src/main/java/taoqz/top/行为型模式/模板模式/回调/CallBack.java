5fcc349c0ef25c7b225807bb169c368a5359b867
taoqingzhou <taoqingzhou@gov110.cn>
2022-11-09 17:22:06 +0800

    add 回调

---
+++ src/main/java/taoqz/top/行为型模式/模板模式/回调/CallBack.java
@@ -0,0 +1,7 @@
+package taoqz.top.行为型模式.模板模式.回调;
+
+public interface CallBack {
+
+    String doMethod();
+
+}
---
+++ src/main/java/taoqz/top/行为型模式/模板模式/回调/MyCallBack.java
@@ -0,0 +1,47 @@
+package taoqz.top.行为型模式.模板模式.回调;
+
+/**
+ * @Author taoqingzhou
+ * @Description
+ * @Date 2022/11/9
+ **/
+public class MyCallBack {
+
+    /**
+     * 模仿 JdbcTemplate query和execute方法
+     * 该方法相当于模板方法,处理逻辑固定,由回调的方式执行
+     *
+     * @param callBack
+     * @return
+     */
+    public String execute(CallBack callBack) {
+
+        String result = callBack.doMethod();
+        if (result.equals("OK")) {
+            return "OK!!!!";
+        }
+        return "fail";
+
+    }
+
+    public String method1(String param1) {
+
+        // 回调的内容相当于要具体实现扩展的部分,根据不同的需要有不同的处理逻辑,最后回调给execute函数即可
+        class Method1Callback implements CallBack {
+
+            @Override
+            public String doMethod() {
+                return param1;
+            }
+
+        }
+
+        return this.execute(new Method1Callback());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MyCallBack().method1("OK"));
+        System.out.println(new MyCallBack().method1("OK1"));
+    }
+
+}
